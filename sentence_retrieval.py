b768f3f358d7ba0cc5dc4e2f5620901a5b317a6d
Jindrich Libovicky <libovicky@ufal.mff.cuni.cz>
2019-10-25 14:28:29 +0200

    EM self-training

--- sentence_retrieval.py
+++ sentence_retrieval.py
@@ -12,6 +12,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from pytorch_pretrained_bert import BertTokenizer, BertModel
+from sklearn.linear_model import LinearRegression
 
 from utils import (
     text_data_generator, batch_generator, get_repr_from_layer, load_bert)
@@ -72,6 +73,9 @@ def main():
     parser.add_argument(
         "--projections", default=None, nargs="+",
         help="List of sklearn projections for particular languages.")
+    parser.add_argument(
+        "--em-iterations", default=None, type=int,
+        help="Iterations of projection self-learning.")
     parser.add_argument("--num-threads", type=int, default=4)
     args = parser.parse_args()
 
@@ -82,6 +86,11 @@ def main():
     if (args.projections is not None and
             len(args.projections) != len(args.data)):
         print("You must have a projection for each data file.",
+              file=sys.stderr)
+        exit(1)
+    if (args.projections is not None and
+            args.em_iterations is not None):
+        print("You either have pre-trained projections or self-train them.",
               file=sys.stderr)
         exit(1)
 
@@ -130,6 +139,21 @@ def main():
 
             representations.append(lng_repr)
 
+        mutual_projections = None
+        if args.em_iterations is not None:
+            print(f"EM training ...")
+            new_mutual_projections = {}
+            for i in range(args.em_iterations):
+                print(f" ... iteration {i + 1}")
+                for lng1, repr1 in zip(args.data, representations):
+                    for lng2, repr2 in zip(args.data, representations):
+                        distances = distance_fn(repr1, repr2)
+                        retrieved = repr2[distances.min(dim=1)[1]]
+                        proj = LinearRegression()
+                        proj.fit(repr1.numpy(), retrieved.numpy())
+                        new_mutual_projections[(lng1, lng2)] = proj
+                mutual_projections = new_mutual_projections
+
         data_len = representations[0].shape[0]
         assert all(r.shape[0] == data_len for r in representations)
         print()
@@ -145,6 +169,11 @@ def main():
             for lng1, repr1 in zip(args.data, representations):
                 print(lng1[-6:-4], end="\t")
                 for lng2, repr2 in zip(args.data, representations):
+
+                    if mutual_projections is not None:
+                        proj = mutual_projections[(lng1, lng2)]
+                        repr1 = torch.from_numpy(proj.predict(repr1.numpy()))
+
                     distances = distance_fn(repr1, repr2)
 
                     recall = recall_at_k_from_distances(distances, k)
