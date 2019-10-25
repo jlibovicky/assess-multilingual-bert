1a8c950b9ff801c97bf164438a4559fabc5f90cb
Jindrich Libovicky <libovicky@ufal.mff.cuni.cz>
2019-10-25 14:34:51 +0200

    explicit projection into QE

--- qe_by_cosine.py
+++ qe_by_cosine.py
@@ -5,7 +5,10 @@
 
 import argparse
 import os
+import logging
+import sys
 
+import joblib
 import numpy as np
 import torch
 import torch.nn as nn
@@ -14,13 +17,13 @@ from pytorch_pretrained_bert import BertTokenizer, BertModel
 from utils import (
     text_data_generator, batch_generator, get_repr_from_layer, load_bert)
 
-import logging
-import sys
 logging.basicConfig(level=logging.INFO)
 
 
-def repr_for_txt_file(filename, tokenizer, model, device, layer, center_lng=True, mean_pool=True):
-    print(f"Processing {filename}", file=sys.stderr)
+def repr_for_txt_file(
+        filename, tokenizer, model, device, layer,
+        center_lng=True, mean_pool=True):
+    print(f"Processing {filename} ... ", file=sys.stderr, end="", flush=True)
     with torch.no_grad():
         vectors = [
             get_repr_from_layer(
@@ -32,7 +35,13 @@ def repr_for_txt_file(filename, tokenizer, model, device, layer, center_lng=True
         lng_repr = torch.cat(vectors, dim=0)
         if center_lng:
             lng_repr = lng_repr - lng_repr.mean(0, keepdim=True)
+    print("Done.", file=sys.stderr)
     return lng_repr
+
+def apply_sklearn_proj(representations, model_path):
+    print("Projecting representations.", file=sys.stderr)
+    model = joblib.load(model_path)
+    return torch.from_numpy(model.predict(representations.numpy()))
 
 
 def main():
@@ -54,8 +63,20 @@ def main():
         help="If true, center representations first.")
     parser.add_argument(
         "--batch-size", type=int, default=32)
+    parser.add_argument(
+        "--src-proj", default=None, type=str,
+        help="Sklearn projection of the source language.")
+    parser.add_argument(
+        "--mt-proj", default=None, type=str,
+        help="Sklearn projection of the target language.")
     parser.add_argument("--num-threads", type=int, default=4)
     args = parser.parse_args()
+
+    if args.center_lng and (
+            args.src_proj is not None and args.src_proj is not None):
+        print("You can either project or center "
+              "the representations, not both.", file=sys.stderr)
+        exit(1)
 
     torch.set_num_threads(args.num_threads)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -68,6 +89,11 @@ def main():
     mt_repr = repr_for_txt_file(
         args.mt, tokenizer, model, device, args.layer,
         center_lng=args.center_lng, mean_pool=args.mean_pool)
+
+    if args.src_proj is not None:
+        src_repr = apply_sklearn_proj(src_repr, args.src_proj)
+    if args.mt_proj is not None:
+        mt_repr = apply_sklearn_proj(mt_repr, args.mt_proj)
 
     src_norm = (src_repr * src_repr).sum(1).sqrt()
     mt_norm = (mt_repr * mt_repr).sum(1).sqrt()
